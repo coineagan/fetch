59dcb2197c85800322d1e528456bc9ba700b90ff
coineagan <37623496+coineagan@users.noreply.github.com>
2024-09-05 19:22:59 -0700

    Add a test for groups of valid and invalid arguments

--- tests/geo.test.js
+++ tests/geo.test.js
@@ -40,6 +40,20 @@ describe('CLI', () => {
     spy.mockRestore();
   })
 
+  test('should handle groups of valid, invalid, and correctly and incorrectly formatted arguments', async () => {
+    const spy = jest.spyOn(console, 'log').mockImplementation();
+    const stdout = await runCLICmd(['60651', 'Portland, OR', '99999', 'Fantasy Land, AA', 'Like... on the moon', '999999999']);
+
+    expect(stdout).toContain('Locality: Portland');
+    expect(stdout).toContain('Locality: Chicago');
+    expect(stdout).toContain('The city/state Fantasy Land, AA could not be found');
+    expect(stdout).toContain('The zip code 99999 could not be found!');
+    expect(stdout).toContain('Like... on the moon" is not in a correct zip code or city/state format');
+    expect(stdout).toContain('999999999" is not in a correct zip code or city/state format');
+
+    spy.mockRestore();
+  });
+
   test('should handle empty arguments', async () => {
     const spy = jest.spyOn(console, 'log').mockImplementation();
     const stdout = await runCLICmd([]);
